9f62e45d783531af674f36f49d87da048bc59737
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2023-02-05 23:39:16 +0100

    upd

--- components/SearchPane/SearchPane.js
+++ components/SearchPane/SearchPane.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import edit from "@/public/edit.svg";
+
 //    TURTLE - TEKI
 //    (°-°) _______
 //      \ / - - - \_
@@ -11,9 +13,20 @@ export default function SearchPane(_) {
     return (
         <div className="h-screen fixed top-0 right-0 transition-transform border-l overflow-y-auto border-l-black bg-black/25 backdrop-blur-xl z-40 py-24 max-w-md" style={{minWidth: "350px"}}>
             <div className="border-b border-b-black py-8">
-                <div className="font-spaceGrotesk pt-2 px-8"><span className="text-xl">BPM:</span> <span className="text-md text-white/60">100-172</span></div>
-                <input type="text" id="search-input" name="search-input" placeholder="100" className="bg-transparent active:text-white text-white/25 hover:text-white focus:text-white px-4 h-8 transition-colors" />
-				<input type="text" id="search-input" name="search-input" placeholder="172" className="bg-transparent active:text-white text-white/25 hover:text-white focus:text-white px-4 h-8 transition-colors" />
+                <div className="px-8">
+                    <div className="font-spaceGrotesk pt-2 text-xl">BPM:</div>
+                    <div className="flex items-baseline gap-x-2">
+                        <div className="flex items-baseline gap-x-0 bg-black/50 border text-center border-white/50 hover:border-white rounded-full px-2 group transition-colors">
+                            <label for="bpm-min"><Image className="h-3" src={ edit } /></label>
+                            <input type="number" id="bpm-min" name="bpm-min" defaultValue="100" min="100" max="172" className="active:text-white text-white/50 group-hover:text-white transition-colors font-spaceGrotesk bg-transparent" />
+                        </div>
+                        <span className="bold">-</span>
+                        <div className="flex items-baseline gap-x-0 bg-black/50 border text-center border-white/50 hover:border-white rounded-full px-2 group transition-colors">
+                            <label for="bpm-min"><Image className="h-3" src={ edit } /></label>
+                            <input type="number" id="bpm-max" name="bpm-max" defaultValue="172" min="100" max="172" className="active:text-white text-white/50 group-hover:text-white transition-colors font-spaceGrotesk bg-transparent" />
+                        </div>
+                    </div>
+                </div>
                 <div className="font-spaceGrotesk pt-2 pb-1 px-8"><span className="text-xl">Keys:</span> <span className="text-md text-white/60">2 selected</span></div>
                 <div className="flex gap-x-2 overflow-x-auto px-8">
                     <div className="bg-white/5 border  border-white rounded-full px-2 text-white transition-colors">A♭m</div>
@@ -43,62 +56,10 @@ export default function SearchPane(_) {
                 <div className="flex gap-x-4 items-center">
                     <Image src="/covers/miss-you.jpg" className="rounded-xl" width={64} height={64} />
                     <div className="flex flex-col justify-start">
-                        <div className="text-xl font-spaceGrotesk p-0">Miss You</div>
+                        <div className="text-xl font-spaceGrotesk p-0">miss you</div>
                         <div className="flex gap-x-4">
-                            <p className="font-spaceGrotesk text-violet-600 hover:text-violet-400 transition-colors">#iayze</p>
-                            <p className="font-spaceGrotesk text-fuchsia-600 hover:text-fuchsia-400 transition-colors">#domcorleo</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-md font-spaceGrotesk text-white/50">1:28</div>
-            </div>
-            <div className="px-8 pb-8 flex justify-between items-center">
-                <div className="flex gap-x-4 items-center">
-                    <Image src="/covers/miss-you.jpg" className="rounded-xl" width={64} height={64} />
-                    <div className="flex flex-col justify-start">
-                        <div className="text-xl font-spaceGrotesk p-0">Miss You</div>
-                        <div className="flex gap-x-4">
-                            <p className="font-spaceGrotesk text-violet-600 hover:text-violet-400 transition-colors">#iayze</p>
-                            <p className="font-spaceGrotesk text-fuchsia-600 hover:text-fuchsia-400 transition-colors">#domcorleo</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-md font-spaceGrotesk text-white/50">1:28</div>
-            </div>
-            <div className="px-8 pb-8 flex justify-between items-center">
-                <div className="flex gap-x-4 items-center">
-                    <Image src="/covers/miss-you.jpg" className="rounded-xl" width={64} height={64} />
-                    <div className="flex flex-col justify-start">
-                        <div className="text-xl font-spaceGrotesk p-0">Miss You</div>
-                        <div className="flex gap-x-4">
-                            <p className="font-spaceGrotesk text-violet-600 hover:text-violet-400 transition-colors">#iayze</p>
-                            <p className="font-spaceGrotesk text-fuchsia-600 hover:text-fuchsia-400 transition-colors">#domcorleo</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-md font-spaceGrotesk text-white/50">1:28</div>
-            </div>
-            <div className="px-8 pb-8 flex justify-between items-center">
-                <div className="flex gap-x-4 items-center">
-                    <Image src="/covers/miss-you.jpg" className="rounded-xl" width={64} height={64} />
-                    <div className="flex flex-col justify-start">
-                        <div className="text-xl font-spaceGrotesk p-0">Miss You</div>
-                        <div className="flex gap-x-4">
-                            <p className="font-spaceGrotesk text-violet-600 hover:text-violet-400 transition-colors">#iayze</p>
-                            <p className="font-spaceGrotesk text-fuchsia-600 hover:text-fuchsia-400 transition-colors">#domcorleo</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-md font-spaceGrotesk text-white/50">1:28</div>
-            </div>
-            <div className="px-8 pb-8 flex justify-between items-center">
-                <div className="flex gap-x-4 items-center">
-                    <Image src="/covers/miss-you.jpg" className="rounded-xl" width={64} height={64} />
-                    <div className="flex flex-col justify-start">
-                        <div className="text-xl font-spaceGrotesk p-0">Miss You</div>
-                        <div className="flex gap-x-4">
-                            <p className="font-spaceGrotesk text-violet-600 hover:text-violet-400 transition-colors">#iayze</p>
-                            <p className="font-spaceGrotesk text-fuchsia-600 hover:text-fuchsia-400 transition-colors">#domcorleo</p>
+                            <p className="font-spaceGrotesk text-violet-600 hover:text-violet-400 transition-colors drop-shadow-tag-violet">#iayze</p>
+                            <p className="font-spaceGrotesk text-fuchsia-600 hover:text-fuchsia-400 transition-colors drop-shadow-tag-fuschia">#domcorleo</p>
                         </div>
                     </div>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
       fontFamily: {
         spaceGrotesk: ['var(--font-spaceGrotesk)', ...defaultTheme.fontFamily.sans],
       },
+      dropShadow: {
+        'tag-violet': '0 0 5px rgb(124 58 237 / 0.75)',
+        'tag-fuschia': '0 0 5px rgb(192 38 211 / 0.75)',
+      }
     },
   },
   plugins: [],
